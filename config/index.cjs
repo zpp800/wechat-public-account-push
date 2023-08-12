18ae623c7cd85d48a4354bd51cfb562be2a6a1c9
zpp <318745470@qq.com>
2023-08-12 23:00:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,6 +24,37 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'SCT219589TDWGnAbnaEndrWoAIfMU4veB6',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0003',
+      
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-08',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '2001', date: '10-24',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '李四', year: '2001', date: '12-08',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-08-04' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '盼盼',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'SCT219245TUlyx6YMAP3uZsnJ8bpXNPXSK',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD

52f0c00150582fb7d70645cba9da90398a70069f
zpp <318745470@qq.com>
2023-08-12 20:25:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,11 +31,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2001', date: '10-24',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '2001', date: '12-08',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',

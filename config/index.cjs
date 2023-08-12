41e8bd75acf3836c23cb44337824386c8c3cb6f0
zpp <318745470@qq.com>
2023-08-12 20:16:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-08-04' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],

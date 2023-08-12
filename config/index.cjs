72cf1b69bc829850e56ff22c4acb5d3825ba853a
zpp <318745470@qq.com>
2023-08-12 23:24:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '涵涵',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT219589TDWGnAbnaEndrWoAIfMU4veB6',
+      id: 'SCT219611TKlm0O6JHZTAZgleBTEBTZHB2',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0003',
       
